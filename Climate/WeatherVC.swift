f41e4ce0b3d6eed7a7db88381d335b0fbff69e44
Murali <kesani.murali@achiever-68.local>
2019-05-17 15:23:33 +0530

    Required Pods Installed

--- Climate/WeatherVC.swift
+++ Climate/WeatherVC.swift
@@ -13,22 +13,16 @@ class WeatherVC: UIViewController {
     @IBOutlet var temperatureLabel: UILabel!
     @IBOutlet var weatherIcon: UIImageView!
     @IBOutlet var cityLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func onButtonTap(_ sender: UIButton)
     {
         var cityVC = self.storyboard?.instantiateViewController(withIdentifier: "CityVC") as! CityVC
