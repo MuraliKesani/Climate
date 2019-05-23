1411c25dfe8df7f056cb656b6f6752ad49133958
Murali <kesani.murali@achiever-68.local>
2019-05-23 12:35:27 +0530

    CLLocationManager class is accessed & P-List is added with LocationPrivacy Settings

--- Climate/WeatherVC.swift
+++ Climate/WeatherVC.swift
@@ -7,26 +7,98 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherVC: UIViewController {
-
+class WeatherVC: UIViewController,CLLocationManagerDelegate {
+   
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "6d3a1dde6c853febcd416fb91044fee60s"
+    
+    //IBOutlets
     @IBOutlet var temperatureLabel: UILabel!
     @IBOutlet var weatherIcon: UIImageView!
     @IBOutlet var cityLabel: UILabel!
-    
-    
+  
+    //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
     }
     
 
     @IBAction func onButtonTap(_ sender: UIButton)
     {
-        var cityVC = self.storyboard?.instantiateViewController(withIdentifier: "CityVC") as! CityVC
+        let cityVC = self.storyboard?.instantiateViewController(withIdentifier: "CityVC") as! CityVC
         self.present(cityVC, animated: true, completion: nil)
     }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+   
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+  
+    
+    
+    //Write the didFailWithError method here:
+   
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+  
+    
+    
+    //Write the PrepareForSegue Method here
     
 }
